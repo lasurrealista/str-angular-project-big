0def50635007730fee9705247ba37d313f8d400f
lasurrealista <ladyblue@freemail.hu>
2021-02-23 18:05:51 +0100

    Filter / sorter pipes (1st version)

---
+++ src/app/pipe/sorter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sorter'
+})
+export class SorterPipe implements PipeTransform {
+
+  transform(list: any[], key: string): any [] {
+
+    if (!Array.isArray(list) || !key) {
+      return list;
+    }
+
+    return list.sort((a, b) =>
+    (typeof a[key] === 'number' && typeof b[key] === 'number') ? a[key] - b[key] :
+      ('' + a[key]).toLowerCase().localeCompare(('' + b[key]).toLowerCase()))
+  }
+}
