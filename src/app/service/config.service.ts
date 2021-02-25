4a42da39084e11f9f5eaa69287eafc52a2c2de33
lasurrealista <ladyblue@freemail.hu>
2021-02-25 14:13:44 +0100

    Product service / Config service (products' table)

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,10 +1,58 @@
 import { Injectable } from '@angular/core';
 
+export interface ITableCol {
+  key: string,
+  title: string,
+  icon?: string,
+  order?: number,
+  pattern?: string,
+  required?: boolean,
+  visible?: boolean,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
   apiUrl: string = 'http://localhost:3000/';
-  constructor() { }
+
+  productsTableColumns: ITableCol[] = [
+    {
+      key: 'id',
+      title: '#',
+    },
+    {
+      key: 'name',
+      title: 'Name',
+    },
+    {
+      key: 'type',
+      title: 'Type',
+    },
+    {
+      key: 'catID',
+      title: 'Category Id',
+    },
+    {
+      key: 'description',
+      title: 'Description',
+    },
+    {
+      key: 'price',
+      title: 'Price',
+    },
+    {
+      key: 'featured',
+      title: 'Featured',
+    },
+    {
+      key: 'active',
+      title: 'Active',
+    }
+
+  ]
+
+  constructor() {
+  }
 }
