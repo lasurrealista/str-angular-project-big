b5db13114cd9eee1077b98fc79511b4da8f96b95
lasurrealista <ladyblue@freemail.hu>
2021-02-25 14:18:59 +0100

    Visibility / Columns of 'products' table

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -21,34 +21,42 @@ export class ConfigService {
     {
       key: 'id',
       title: '#',
+      visible: true,
     },
     {
       key: 'name',
       title: 'Name',
+      visible: true,
     },
     {
       key: 'type',
       title: 'Type',
+      visible: true,
     },
     {
       key: 'catID',
       title: 'Category Id',
+      visible: true,
     },
     {
       key: 'description',
       title: 'Description',
+      visible: true,
     },
     {
       key: 'price',
       title: 'Price',
+      visible: true,
     },
     {
       key: 'featured',
       title: 'Featured',
+      visible: true,
     },
     {
       key: 'active',
       title: 'Active',
+      visible: true,
     }
 
   ]
