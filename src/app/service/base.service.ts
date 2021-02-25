f5c9b3bf8c48353f56131aa39ef4731c28d4b2a2
Plecs <plecs@t-online.hu>
2021-02-25 14:19:03 +0100

    Base service has been completed

--- src/app/service/base.service.ts
+++ src/app/service/base.service.ts
@@ -42,4 +42,17 @@ export class BaseService<T extends { id: number}> {
       )
     )
   }
+
+  update(entity: T): Observable<T> {
+    return this.http.patch<T>(
+      `${this.config.apiUrl}/${this.entityName}/${entity.id}`,
+      entity);
+  }
+
+  remove(entity: T): Observable<T> {
+    return this.http.delete<T>(
+      `${this.config.apiUrl}/${this.entityName}/${entity.id}`,
+    );
+  }
+
 }
