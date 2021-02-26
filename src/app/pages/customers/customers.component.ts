ab0bab67c57f836e0e40298af7dd0539d9a4963d
petert360 <73585032+petert360@users.noreply.github.com>
2021-02-26 02:13:23 +0100

    add: cols to CustomerComponent

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from 'src/app/model/customer';
 import { CustomerService } from 'src/app/service/customer.service';
 import { Observable } from 'rxjs';
+import { ConfigService, ITableCol } from 'src/app/service/config.service';
 
 @Component({
   selector: 'app-customers',
@@ -10,9 +11,12 @@ import { Observable } from 'rxjs';
 })
 export class CustomersComponent implements OnInit {
 
-  customers$: Observable<Customer[]> = this.customerService.list$
+  customers$: Observable<Customer[]> = this.customerService.list$;
+  cols: ITableCol[] = this.config.customersTableColumns;
+  
   constructor(
     private customerService: CustomerService,
+    private config: ConfigService,
   ) { }
 
   ngOnInit(): void {
