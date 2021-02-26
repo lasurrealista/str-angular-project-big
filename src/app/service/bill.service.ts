6029c9292b63ea6c1595347282e66060078bb4b3
djidji3 <djidji@freemail.hu>
2021-02-26 17:58:40 +0100

    create bill service

---
+++ src/app/service/bill.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Bill } from '../model/bill';
+import { Customer } from '../model/customer';
+import { BaseService } from './base.service';
+import { ConfigService } from './config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillService extends BaseService<Bill> {
+
+  list$: BehaviorSubject<Bill[]> = new BehaviorSubject<Bill[]>([]);
+  
+  constructor(
+    public config: ConfigService,
+    public http: HttpClient,
+  ) {
+    super(http, config, 'bill');
+  }
+}
+
+
