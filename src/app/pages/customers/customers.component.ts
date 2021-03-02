3d99321315a9ca55257ebfb33ded905f4f4eaec0
petert360 <73585032+petert360@users.noreply.github.com>
2021-03-02 21:45:28 +0100

    update: CustomerComponent - new SortPipe

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -96,9 +96,12 @@ export class CustomersComponent implements OnInit {
   }
 
   columnKey: string = '';
+  sortDir: number = -1;
 
   onColumnSelect(key: string): void {
+    
     this.columnKey = key;
+    this.sortDir = this.sortDir * (-1);
   }
 }
 
