4c1dff133512b9ff2bebf4012b1137d1da1e5cea
petert360 <73585032+petert360@users.noreply.github.com>
2021-03-02 21:14:25 +0100

    insert: sorter pipe code

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -37,7 +37,7 @@ export class SorterPipe implements PipeTransform {
 
 export class SorterPipe implements PipeTransform {
 
-  transform(list: any[] | null, key: string): any [] | null {
+/*   transform(list: any[] | null, key: string): any [] | null {
 
     if (!Array.isArray(list) || !key) {
       return list;
@@ -47,5 +47,27 @@ export class SorterPipe implements PipeTransform {
     (typeof a[key] === 'number' && typeof b[key] === 'number') ? a[key] - b[key] :
       ('' + a[key]).toLowerCase().localeCompare(('' + b[key]).toLowerCase()))
   }
+ */
+
+transform(value: any[] | null, key: string, dir: number = 1): any[] | null {
+  if (!Array.isArray(value) || !key) {
+    return value;
+  }
+  return value.sort( (a, b) => {
+    if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+      return (a[key] - b[key]) * dir;
+    } else {
+      return (
+        ('' + a[key])
+          .toLowerCase()
+          .localeCompare(
+            ('' + b[key]).toLowerCase()
+          )
+        ) * dir;
+    }
+  });
+}
+
+
 }
 
